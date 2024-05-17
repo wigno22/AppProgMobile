34e3b820233b9c97b320fd08773504731f982b87
Walter <wsigno2002@gmail.com>
2024-05-17 11:23:54 +0200

    Creato struttura del database, creazione bottombar, sistemare immagini

--- app/src/main/java/com/example/progettoprogrammazionemobile/MainActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazionemobile/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.progettoprogrammazionemobile
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import java.lang.Math.random
+import java.security.SecureRandom
+import java.time.LocalDateTime
+import java.util.*
 
 
-data class Account (val nome: String, val valore: Double, val direction: Boolean )
+data class AccountDetails (val iDNumber: String , val IBAN: String, val Balance: Number){}
+data class Transaction (val iDNumber: String , val Valore: Number, val Type: Boolean, val Category: String, val Description: String, val Data: String ){}
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,30 +51,62 @@ class MainActivity : AppCompatActivity() {
 
 
         val transaction: Button = findViewById(R.id.transaction_button)
+        /*
+                transaction.setOnClickListener {
+                    val currentUser = firebaseAuth.currentUser
 
-        transaction.setOnClickListener {
-            val currentUser = firebaseAuth.currentUser
+                    val CountValue = db.collection(currentUser.toString()).document("my_Account")
 
-            val CountValue = db.collection(currentUser.toString()).document("my_Account")
-
-            CountValue.update("grade", FirebaseFirestore.FieldValue.increment(10))
-        }
-
+                    //CountValue.update("grade", FirebaseFirestore.FieldValue.increment(10))
+                }
+        */
 
 
         db = Firebase.firestore
         firebaseAuth = FirebaseAuth.getInstance()
 
-        val AccountList = mutableListOf<Account>()
-        AccountList.add(Account("ProgMob", 10.5 , true))
 
-        val grades = hashMapOf("Account" to AccountList )
+        val codiceConto = SecureRandom().nextInt(90000000) + 10000000
 
-        //test Content-owner only access
-        db.collection(firebaseAuth.currentUser!!.uid).document("my_Account").set(grades)
+        fun generateIban(random: SecureRandom): String {
+            val countryCode = "IT"
+            val bankCode = "12345"
+            val accountNumber = codiceConto
+            val controlDigits = random.nextInt(900) + 100
+            return "$countryCode$controlDigits$bankCode$accountNumber"
+        }
+
+        val user = firebaseAuth.currentUser;
+        val UID = user!!.uid
+        val iban = generateIban(SecureRandom())
+        var Date = LocalDateTime.now()
+
+        val categorieValide = listOf("food", "transport", "shopping", "service", "entertainment", "salary", "household expenses", "subscription")
+       // val AccountDetails = mutableListOf<AccountDetails>()
+       // AccountDetails.add(AccountDetails(user, iban, 0))
+
+        val Transactions = hashMapOf<String,Transaction>()
+
+        val categoria = "food" // Questo dovrebbe essere il valore che stai cercando di impostare
+        if (categoria in categorieValide) {
+
+            Transactions.put(Date.toString() ,Transaction( UID ,3, false , categoria, " ", Date.toString() ))
+            Date = Date.plusDays(2)
+            Transactions.put(Date.toString() ,Transaction( UID ,4, false , categoria, " ", Date.toString() ))
+
+        }
+        else
+        {
+            Log.w(TAG, "Categoria non valida: $categoria")
+        }
+
+
+        Transactions.map {
+            db.collection(firebaseAuth.currentUser!!.uid).document("Account").collection("Transaction").document(it.key).set(it.value)
+        }
+
+        db.collection(firebaseAuth.currentUser!!.uid).document("Account").set(AccountDetails(UID, iban, 0))
     }
-
-
 
 
 }
