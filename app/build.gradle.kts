59d4edea0ac15e7991b00f2f27bab513e2a0e1d1
Walter <wsigno2002@gmail.com>
2024-05-21 00:14:11 +0200

    aggiornato cambiamenti di fragment tramite bottombar, gestire i 3 fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,8 @@ android {
     namespace = "com.example.progettoprogrammazionemobile"
     compileSdk = 34
 
+
+
     defaultConfig {
         applicationId = "com.example.progettoprogrammazionemobile"
         minSdk = 33
@@ -34,6 +36,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
@@ -54,4 +57,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val nav_version = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- app/src/main/java/com/example/progettoprogrammazionemobile/MainActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazionemobile/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.progettoprogrammazionemobile
 
 import android.annotation.SuppressLint
+import android.content.ClipData.Item
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +10,10 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
@@ -17,20 +22,40 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.security.SecureRandom
 import java.time.LocalDateTime
+import java.lang.Override as Override
 
 data class AccountDetails(val iDNumber: String, val IBAN: String, val Balance: Number)
 data class Transaction(val iDNumber: String, val Valore: Number, val Type: Boolean, val Category: String, val Description: String, val Data: String)
 
+class profilefrg: Fragment(R.layout.fragment_profile)
+class accountfrg: Fragment(R.layout.fragment_account)
+class operationfrg: Fragment(R.layout.fragment_operation)
 class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textName: TextView = findViewById(R.id.textView)
+        textName.text = "Benvenuto " + intent.extras?.getString("USER")
+
+        //val navController = findNavController(R.id.nav_host)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
+        //navController.navigate(R.id.action_accountFragment_to_operationFragment)
+
+
+/*
         val signOutButton: Button = findViewById(R.id.sign_out)
         signOutButton.setOnClickListener {
             AuthUI.getInstance()
@@ -42,49 +67,10 @@ class MainActivity : AppCompatActivity() {
                 }
         }
 
-        val textName: TextView = findViewById(R.id.textView)
-        textName.text = "Benvenuto " + intent.extras?.getString("USER")
-
-        val buttonProfilo: ImageButton = findViewById(R.id.ButtonProfilo)
-        val buttonOperazioni: ImageButton = findViewById(R.id.ButtonOperazioni)
-        val buttonConto: ImageButton = findViewById(R.id.ButtonConto)
-
-        buttonProfilo.setOnClickListener {
-            loadFragment(ProfileFragment())
-        }
-
-        buttonOperazioni.setOnClickListener {
-            loadFragment(OperationFragment())
-        }
-
-        buttonConto.setOnClickListener {
-            loadFragment(AccountFragment())
-        }
-
+*/
         // Bottom Navigation
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_profile -> {
-                    loadFragment(ProfileFragment())
-                    true
-                }
-                R.id.navigation_operations -> {
-                    loadFragment(OperationFragment())
-                    true
-                }
-                R.id.navigation_account -> {
-                    loadFragment(AccountFragment())
-                    true
-                }
-                else -> false
-            }
-        }
+        //val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        // Carica il fragment di default
-        if (savedInstanceState == null) {
-            loadFragment(ProfileFragment())
-        }
 
         db = Firebase.firestore
         firebaseAuth = FirebaseAuth.getInstance()
@@ -108,7 +94,7 @@ class MainActivity : AppCompatActivity() {
 
         val transactions = hashMapOf<String, Transaction>()
 
-        val categoria = "food" // Questo dovrebbe essere il valore che stai cercando di impostare
+        val categoria = "food"
         if (categoria in categorieValide) {
             transactions[date.toString()] = Transaction(UID, 3, false, categoria, " ", date.toString())
             date = date.plusDays(2)
@@ -124,10 +110,4 @@ class MainActivity : AppCompatActivity() {
         db.collection(firebaseAuth.currentUser!!.uid).document("Account").set(AccountDetails(UID, iban, 0))
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .commit()
-    }
 }
